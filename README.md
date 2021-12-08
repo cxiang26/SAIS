4a14d0117a6ea36b2ebd4a1372262c276612b3ab
shaun <cq.xiang@foxmail.com>
2021-12-08 22:12:54 +0800

    ''

--- README.md
+++ README.md
@@ -6,18 +6,25 @@ This is an official release of the paper **SAIS: Single-stage Anchor-free Instan
 Global-based and Local-based instance encoding methods are implemented.
 ## Results
 
-### Instance Segmentation on COCO
+### Instance Segmentation on COCO （FPS including forward and whole post-processing）
 
-| Backbone | Method | Lr Schd | Mask mAP| Config | Download |
-| :---: | :---: | :---: | :---: | :---: | :---: |
-
+| Method      | Backbone      | Lr Schd | Mask mAP| mAP@S | mAP@L | FPS   |
+| :---:       | :---:         | :---:   | :---:   | :---: | :---: | :---: |
+| SAIS-L-544  | ResNet-50-FPN |  2x     | 29.2    | 10.7  | 38.2  | 34.0  |
+| SAIS-G-544  | ResNet-50-FPN |  2x     | 27.2    | 7.5   | 46.4  | 38.6  |
+| SAIS-GL-544 | ResNet-50-FPN |  2x     | 28.6    | 7.8   | 50.3  | 38.6  |
+| :---:       | :---:         | :---:   | :---:   | :---: | :---: | :---: |
+| SAIS-L-704  | ResNet-101-FPN | 3x     | 33.7    | 14.0  | 52.1  | 26.4  |
+| SAIS-G-704  | ResNet-101-FPN | 3x     | 32.8    | 11.1  | 52.6  | 28.6  |
+| SAIS-GL-704 | ResNet-101-FPN | 3x     | 33.0    | 12.2  | 53.0  | 28.6  |
+ 
 ## Installation
 
 It requires the following OpenMMLab packages:
 
-- MIM >= 0.1.5
-- MMCV-full >= v1.3.14
-- MMDetection >= v2.17.0
+- MIM = 0.1.5
+- MMCV-full = v1.3.14
+- MMDetection = v2.17.0
 - scipy
 
 ```bash
---
+++ configs/sais-global/saisg++_r101_caffe_fpn_3x_704.py
@@ -0,0 +1,74 @@
+_base_ =  './saisg++_r50_caffe_fpn_2x.py'
+
+model = dict(
+    backbone=dict(
+        depth=101,
+        init_cfg=dict(
+            type='Pretrained',
+            checkpoint='open-mmlab://detectron/resnet101_caffe')))
+
+# dataset settings
+dataset_type = 'CocoDataset'
+data_root = 'data/coco/'
+img_norm_cfg = dict(
+    mean=[102.9801, 115.9465, 122.7717], std=[1.0, 1.0, 1.0], to_rgb=False)
+train_pipeline = [
+    dict(type='LoadImageFromFile', to_float32=True),
+    dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
+    dict(
+        type='PhotoMetricDistortion',
+        brightness_delta=32,
+        contrast_range=(0.5, 1.5),
+        saturation_range=(0.5, 1.5),
+        hue_delta=18),
+    dict(
+        type='Expand',
+        mean=img_norm_cfg['mean'],
+        to_rgb=img_norm_cfg['to_rgb'],
+        ratio_range=(1, 4)),
+    dict(
+        type='MinIoURandomCrop',
+        min_ious=(0.1, 0.3, 0.5, 0.7, 0.9),
+        min_crop_size=0.3),
+    dict(type='Resize', img_scale=(768, 768), keep_ratio=False),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks']),
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(704, 704),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=False),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+
+data = dict(
+    samples_per_gpu=16,
+    workers_per_gpu=4,
+    train=dict(
+        _delete_=True,
+        type='RepeatDataset',
+        times=3,
+        dataset=dict(
+            type=dataset_type,
+            ann_file=data_root + 'annotations/instances_train2017.json',
+            img_prefix=data_root + 'train2017/',
+            pipeline=train_pipeline)),
+    val=dict(
+        type=dataset_type,
+        ann_file=data_root + 'annotations/instances_val2017.json',
+        img_prefix=data_root + 'val2017/',
+        pipeline=test_pipeline),
+    test=dict(
+        type=dataset_type,
+        ann_file=data_root + 'annotations/instances_val2017.json',
+        img_prefix=data_root + 'val2017/',
+        pipeline=test_pipeline))
